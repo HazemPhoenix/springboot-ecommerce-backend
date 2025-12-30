707cee7f43c0f197f78225953042a0d5f46bb897
HazemPhoenix <hazem.mando408@gmail.com>
2025-12-30 18:07:35 +0200

    Feat(ErrorResponse, GlobalExceptionHandler): create an ErrorResponse class for a unified response structure

---
+++ src/main/java/io/spring/training/boot/server/DTOs/ErrorResponse.java
@@ -0,0 +1,22 @@
+package io.spring.training.boot.server.DTOs;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class ErrorResponse {
+    private Instant timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.timestamp = Instant.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
--- src/main/java/io/spring/training/boot/server/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/spring/training/boot/server/exceptions/GlobalExceptionHandler.java
@@ -1,17 +1,24 @@
 package io.spring.training.boot.server.exceptions;
 
+import io.spring.training.boot.server.DTOs.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleBookNotFound(BookNotFoundException exception){
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleBookNotFound(BookNotFoundException exception, WebRequest request){
+        return new ResponseEntity<>(new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                exception.getMessage(),
+                request.getDescription(false)
+        ), HttpStatus.NOT_FOUND);
     }
 }
