caab5e90f9672526b6fcfbab05fef1666570d153
HazemPhoenix <hazem.mando408@gmail.com>
2025-12-30 15:50:33 +0200

    initial commit

---
+++ src/test/java/io/spring/training/boot/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.training.boot.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
