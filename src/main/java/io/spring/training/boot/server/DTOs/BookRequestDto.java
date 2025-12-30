1582e23918b5882bbd4bbd3afd41ec487244bcae
HazemPhoenix <hazem.mando408@gmail.com>
2025-12-30 21:52:14 +0200

    Update(GlobalExceptionHandler, BookRequestDto): improve valiation error response message, add null checks for the price and no. of pages in BookRequestDto

--- src/main/java/io/spring/training/boot/server/DTOs/BookRequestDto.java
+++ src/main/java/io/spring/training/boot/server/DTOs/BookRequestDto.java
@@ -1,13 +1,14 @@
 package io.spring.training.boot.server.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record BookRequestDto(@NotBlank(message = "Title is required") String title,
                              @NotBlank(message = "Description is required") String description,
-                             @Positive(message = "Price cannot be a negative value") BigDecimal price,
-                             @Positive(message = "Number of pages cannot be negative") int numberOfPages,
+                             @NotNull(message = "Price is required") @Positive(message = "Price cannot be a negative value") BigDecimal price,
+                             @NotNull(message = "Number of pages is required") @Positive(message = "Number of pages cannot be negative") Integer numberOfPages,
                              String image) {
 }
--- src/main/java/io/spring/training/boot/server/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/spring/training/boot/server/exceptions/GlobalExceptionHandler.java
@@ -3,30 +3,48 @@ package io.spring.training.boot.server.exceptions;
 import io.spring.training.boot.server.DTOs.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleBookNotFound(BookNotFoundException exception, WebRequest request){
-        return formatErrorResponse(HttpStatus.NOT_FOUND, exception, request);
+        return formatErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage(), request);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleInvalidData(MethodArgumentNotValidException exception, WebRequest request){
-        return formatErrorResponse(HttpStatus.UNPROCESSABLE_CONTENT, exception, request);
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        StringBuilder messageBuilder = new StringBuilder();
+        errors.forEach((name, msg) -> {
+            messageBuilder.append(name).append(": ").append(msg).append(", ");
+        });
+        String message = messageBuilder.substring(0, messageBuilder.lastIndexOf(","));
+
+        return formatErrorResponse(HttpStatus.UNPROCESSABLE_CONTENT, message, request);
     }
 
-    private ResponseEntity<ErrorResponse> formatErrorResponse(HttpStatus status, Exception exception, WebRequest request){
+    private ResponseEntity<ErrorResponse> formatErrorResponse(HttpStatus status, String message, WebRequest request){
         return new ResponseEntity<>(new ErrorResponse(
                 status.value(),
                 status.getReasonPhrase(),
-                exception.getMessage(),
+                message,
                 request.getDescription(false)
         ), status);
     }
