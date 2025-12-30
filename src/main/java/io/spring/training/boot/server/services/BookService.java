4652b007c243dc631cf0c0f574f2e3315e8afa88
HazemPhoenix <hazem.mando408@gmail.com>
2025-12-30 17:01:58 +0200

    Feat: create basic BookService

---
+++ src/main/java/io/spring/training/boot/server/services/BookService.java
@@ -0,0 +1,24 @@
+package io.spring.training.boot.server.services;
+
+import io.spring.training.boot.server.models.Book;
+import lombok.AllArgsConstructor;
+import lombok.Lombok;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class BookService {
+    private final BookRepo bookRepo;
+
+    public Book createBook(Book book) {
+        return bookRepo.save(book);
+    }
+
+    public Book findBookById(int id){
+        Optional<Book> book = bookRepo.findById(id);
+
+        return book.orElse(new Book());
+    }
+}
