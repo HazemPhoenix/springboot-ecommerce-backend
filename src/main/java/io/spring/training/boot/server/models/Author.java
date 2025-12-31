1b314e69e23e6b0cf83ea4fdcca4254f91bea9cb
HazemPhoenix <hazem.mando408@gmail.com>
2025-12-31 13:07:23 +0200

    Feat(Author): create Author model

---
+++ src/main/java/io/spring/training/boot/server/models/Author.java
@@ -0,0 +1,21 @@
+package io.spring.training.boot.server.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "authors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String bio;
+    private String nationality;
+    private String photo;
+}
