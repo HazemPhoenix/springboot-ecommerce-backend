ca5a4e305d22b9944af93c949be604d4857a91ff
HazemPhoenix <hazem.mando408@gmail.com>
2025-12-31 16:34:36 +0200

    Feat(application.properties, BookRepo): apply a default batch fetch size to mitigate n+1 problem without loading all books in memory first before pagination (which would happen if we use JOIN FETCH or EntityGraph

--- src/main/java/io/spring/training/boot/server/repositories/BookRepo.java
+++ src/main/java/io/spring/training/boot/server/repositories/BookRepo.java
@@ -1,9 +1,19 @@
 package io.spring.training.boot.server.repositories;
 
 import io.spring.training.boot.server.models.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
+    Page<Book> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"authors"})
+    Optional<Book> findById(Long id);
 }
