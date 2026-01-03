64b104a85d8e01e2ec03b0b1775258cbc8958406
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-03 14:44:04 +0200

    Feat(AuthorRepo): use EntityGraph in AuthorRepo to eagerly load the author genres when requesting a specific author by ID

--- src/main/java/io/spring/training/boot/server/repositories/AuthorRepo.java
+++ src/main/java/io/spring/training/boot/server/repositories/AuthorRepo.java
@@ -1,7 +1,12 @@
 package io.spring.training.boot.server.repositories;
 
 import io.spring.training.boot.server.models.Author;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepo extends JpaRepository<Author, Long> {
+    @EntityGraph(attributePaths = "genres")
+    Optional<Author> findById(Long id);
 }
