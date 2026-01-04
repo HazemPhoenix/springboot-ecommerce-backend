0af381e4840bbfb4d9a301f56f9381717b3b53c5
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-04 15:53:02 +0200

    Feat(User): create User model

---
+++ src/main/java/io/spring/training/boot/server/models/User.java
@@ -0,0 +1,31 @@
+package io.spring.training.boot.server.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private boolean active;
+    @Column(name = "phone")
+    private String phoneNumber;
+    private boolean verified;
+
+    public User(String username, String password, String email, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
