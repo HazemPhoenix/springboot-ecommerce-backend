d6025818db965020509f458297931766f22b3149
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-04 16:13:57 +0200

    Feat(UserController): create initial User controller and register endpoint

---
+++ src/main/java/io/spring/training/boot/server/controllers/UserController.java
@@ -0,0 +1,29 @@
+package io.spring.training.boot.server.controllers;
+
+import io.spring.training.boot.server.models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> registerUser(@RequestBody UserRequestDto requestDto){
+        UserResponseDto responseDto = userService.registerUser(requestDto);
+        URI userUri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{userId}")
+                .build(responseDto.id());
+        ResponseEntity.created(userUri).body(responseDto);
+    }
+}
