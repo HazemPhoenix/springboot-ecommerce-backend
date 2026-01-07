8e3db7fbd3f8cf9b92738269783e2bc3983557f6
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-07 18:11:21 +0200

    Test(AuthorServiceImplTest): test createAuthor method

--- src/test/java/io/spring/training/boot/server/services/AuthorServiceImplTest.java
+++ src/test/java/io/spring/training/boot/server/services/AuthorServiceImplTest.java
@@ -1,6 +1,8 @@
 package io.spring.training.boot.server.services;
 
+import io.spring.training.boot.server.DTOs.author.AuthorRequestDto;
 import io.spring.training.boot.server.DTOs.author.AuthorResponseDto;
+import io.spring.training.boot.server.DTOs.genre.GenreResponseDto;
 import io.spring.training.boot.server.config.StorageProperties;
 import io.spring.training.boot.server.exceptions.AuthorNotFoundException;
 import io.spring.training.boot.server.models.Author;
@@ -8,6 +10,7 @@ import io.spring.training.boot.server.models.Book;
 import io.spring.training.boot.server.models.Genre;
 import io.spring.training.boot.server.repositories.AuthorRepo;
 import io.spring.training.boot.server.services.implementations.AuthorServiceImpl;
+import io.spring.training.boot.server.services.implementations.FileSystemImageStorageService;
 import io.spring.training.boot.server.services.implementations.GenreServiceImpl;
 import io.spring.training.boot.server.utils.mappers.AuthorMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,8 +24,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -35,6 +42,8 @@ public class AuthorServiceImplTest {
     private AuthorRepo authorRepo;
     @Mock
     private GenreServiceImpl genreService;
+    @Mock
+    private FileSystemImageStorageService imageStorageService;
 
     @InjectMocks
     private AuthorServiceImpl authorService;
@@ -115,5 +124,45 @@ public class AuthorServiceImplTest {
         verify(authorRepo).findById(10L);
 
     }
+
+    @Test
+    public void givenCorrectRequestDtoAndImageFile_whenCreateAuthorIsCalled_thenReturnsCorrectAuthorResponseDto(){
+        // Arrange
+        String photo = "image.png";
+        when(imageStorageService.storeAuthorImage(any(MultipartFile.class))).thenReturn(photo);
+
+        Set<Genre> genres = Set.of(new Genre("Horror"), new Genre("Drama"));
+        when(genreService.findGenresByIds(any(Set.class))).thenReturn(genres);
+
+        Author author = Author.builder()
+                .id(3L)
+                .photo(photo)
+                .genres(genres)
+                .name("test")
+                .bio("test")
+                .nationality("test")
+                .build();
+
+        when(authorRepo.save(any(Author.class))).thenReturn(author);
+
+        // Act
+        AuthorRequestDto authorRequestDto = new AuthorRequestDto("test", "test", "test", Set.of(1L, 2L));
+        MultipartFile multipartFile = new MockMultipartFile(photo, new byte[]{1,2,3});
+        AuthorResponseDto authorResponseDto = authorService.createAuthor(authorRequestDto, multipartFile);
+
+        // Assert
+        verify(imageStorageService).storeAuthorImage(multipartFile);
+        verify(genreService).findGenresByIds(Set.of(1L, 2L));
+        verify(authorRepo).save(any(Author.class));
+
+        assertThat(authorResponseDto).isNotNull();
+        assertThat(authorResponseDto.id()).isEqualTo(3L);
+        assertThat(authorResponseDto.name()).isEqualTo("test");
+        assertThat(authorResponseDto.bio()).isEqualTo("test");
+        assertThat(authorResponseDto.nationality()).isEqualTo("test");
+        assertThat(authorResponseDto.genres().stream().map(GenreResponseDto::name).toList()).containsAll(List.of("Horror", "Drama"));
+        assertThat(authorResponseDto.photo()).isEqualTo("http://localhost:8080//uploads/image.png");
+    }
+
 
 }
