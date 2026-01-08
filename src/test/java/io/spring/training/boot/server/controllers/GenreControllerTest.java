c9327ced394247b7f2d8e9c4f33b7c328ea04e81
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-08 17:16:32 +0200

    Test(GenreControllerTest): test getGenreById method

--- src/test/java/io/spring/training/boot/server/controllers/GenreControllerTest.java
+++ src/test/java/io/spring/training/boot/server/controllers/GenreControllerTest.java
@@ -1,8 +1,10 @@
 package io.spring.training.boot.server.controllers;
 
 
+import io.spring.training.boot.server.DTOs.error.ErrorResponse;
 import io.spring.training.boot.server.DTOs.genre.GenreResponseDto;
 import io.spring.training.boot.server.config.StorageProperties;
+import io.spring.training.boot.server.exceptions.GenreNotFoundException;
 import io.spring.training.boot.server.models.Genre;
 import io.spring.training.boot.server.services.GenreService;
 import io.spring.training.boot.server.utils.mappers.GenreMapper;
@@ -10,12 +12,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
-import static org.mockito.ArgumentMatchers.any;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -54,7 +58,7 @@ public class GenreControllerTest {
     }
 
     @Test
-    public void givenExistingGenres_whenGetAllGenresIsCalled_thenReturnAListOfGenreResponseDtos() throws Exception {
+    public void givenExistingGenres_whenGetAllGenresIsCalled_thenReturnsAListOfGenreResponseDtos() throws Exception {
         // Arrange
         List<GenreResponseDto> response = genres.stream().map(GenreMapper::toGenreResponseDto).toList();
         when(genreService.getAllGenres()).thenReturn(response);
@@ -68,6 +72,37 @@ public class GenreControllerTest {
                 .andExpect(jsonPath("$[0].name").value("Horror"))
                 .andExpect(jsonPath("$[1].name").value("Drama"));
 
+    }
+
+    @Test
+    public void givenValidGenreId_whenGetGenreByIdIsCalled_thenReturnsCorrectGenreResponseDto() throws Exception {
+        // Arrange
+        Long id = 1L;
+        GenreResponseDto response = GenreMapper.toGenreResponseDto(genres.get(0));
+
+        when(genreService.findGenreById(anyLong())).thenReturn(response);
+
+        // Act & Assert
+        mockMvc.perform(get(baseUrl + "/" + id))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Horror"));
+    }
+
+    @Test
+    public void givenInvalidId_whenGetGenreByIdIsCalled_thenReturnNotFoundResponse() throws Exception {
+        // Arrange
+        Long id = 10L;
+
+        when(genreService.findGenreById(anyLong())).thenThrow(GenreNotFoundException.class);
+
+        // Act & Assert
+        mockMvc.perform(get(baseUrl + "/" + id))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value(HttpStatus.NOT_FOUND.value()))
+                .andExpect(jsonPath("$.error").value(HttpStatus.NOT_FOUND.getReasonPhrase()));
     }
 
 
