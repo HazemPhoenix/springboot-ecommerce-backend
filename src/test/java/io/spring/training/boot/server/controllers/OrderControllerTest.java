b261e89fce755bbbf24472439fd53a189a718d62
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-10 15:42:04 +0200

    Test(OrderControllerTest): test createOrder method

---
+++ src/test/java/io/spring/training/boot/server/controllers/OrderControllerTest.java
@@ -0,0 +1,167 @@
+package io.spring.training.boot.server.controllers;
+
+import io.spring.training.boot.server.DTOs.order.OrderItemRequestDto;
+import io.spring.training.boot.server.DTOs.order.OrderRequestDto;
+import io.spring.training.boot.server.DTOs.order.OrderUserResponseDto;
+import io.spring.training.boot.server.config.StorageProperties;
+import io.spring.training.boot.server.models.*;
+import io.spring.training.boot.server.models.enums.OrderStatus;
+import io.spring.training.boot.server.models.enums.PaymentMethod;
+import io.spring.training.boot.server.services.BookService;
+import io.spring.training.boot.server.services.OrderService;
+import io.spring.training.boot.server.utils.mappers.OrderItemMapper;
+import io.spring.training.boot.server.utils.mappers.OrderMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private StorageProperties storageProperties;
+
+    @MockitoBean
+    private OrderService orderService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private List<Order> orders;
+
+    private final String baseUrl = "/api/v1/orders";
+
+    @BeforeEach
+    public void setup(){
+        Author a1 = Author.builder()
+                .id(1L)
+                .name("first author")
+                .bio("first bio")
+                .nationality("first nat")
+                .genres(Set.of(new Genre("Horror")))
+                .photo("first.png").build();
+
+        Author a2 = Author.builder()
+                .id(2L)
+                .name("second author")
+                .bio("second bio")
+                .nationality("second nat")
+                .genres(Set.of(new Genre("Drama")))
+                .photo("second.png").build();
+
+        Book b1 = Book.builder()
+                .id(1L)
+                .title("Book One")
+                .description("Description One")
+                .price(new BigDecimal("29.99"))
+                .numberOfPages(300)
+                .stock(10)
+                .image("book-one.png")
+                .authors(Set.of(a1))
+                .genres(Set.of(new Genre("Horror")))
+                .build();
+
+        Book b2 = Book.builder()
+                .id(2L)
+                .title("Book Two")
+                .description("Description Two")
+                .price(new BigDecimal("39.99"))
+                .numberOfPages(400)
+                .stock(5)
+                .image("book-two.png")
+                .authors(Set.of(a2))
+                .genres(Set.of(new Genre("Drama")))
+                .build();
+
+        Order o1 = null;
+        OrderItem oi1 = OrderItem.builder()
+                .id(1L)
+                .order(o1)
+                .book(b1)
+                .quantity(5)
+                .totalItemPrice(BigDecimal.valueOf(5).multiply(BigDecimal.valueOf(29.99)))
+                .build();
+
+        o1 = Order.builder()
+                .id(1L)
+                .user(new User())
+                .status(OrderStatus.PROCESSING)
+                .paymentMethod(PaymentMethod.COD)
+                .orderItems(Set.of(oi1))
+                .totalAmount(oi1.getTotalItemPrice())
+                .date(LocalDate.now())
+                .build();
+
+        Order o2 = null;
+        OrderItem oi2 = OrderItem.builder()
+                .id(2L)
+                .order(o2)
+                .book(b2)
+                .quantity(2)
+                .totalItemPrice(BigDecimal.valueOf(2).multiply(BigDecimal.valueOf(39.99)))
+                .build();
+
+        o2 = Order.builder()
+                .id(2L)
+                .user(new User())
+                .status(OrderStatus.PROCESSING)
+                .paymentMethod(PaymentMethod.CREDIT_CARD)
+                .orderItems(Set.of(oi2))
+                .totalAmount(oi2.getTotalItemPrice())
+                .date(LocalDate.now())
+                .build();
+
+        orders = List.of(o1, o2);
+    }
+
+    @Test
+    public void givenValidOrderRequestDto_whenCreateOrderIsCalled_thenReturnsOrderUserResponseDto() throws Exception {
+        // arrange
+        Order order = orders.get(0);
+        OrderRequestDto request = new OrderRequestDto(PaymentMethod.COD, order.getOrderItems().stream().map(oi -> new OrderItemRequestDto(oi.getBook().getId(), oi.getQuantity())).toList());
+        OrderUserResponseDto response = OrderMapper.toOrderUserResponseDto(order);
+
+        when(orderService.createOrder(any(OrderRequestDto.class))).thenReturn(response);
+
+        // act and assert
+        mockMvc.perform(post(baseUrl)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(response)));
+
+        verify(orderService).createOrder(any(OrderRequestDto.class));
+    }
+
+    @Test
+    public void givenInvalidOrderRequestDto_whenCreateOrderIsCalled_thenReturnsUnProcessableContent() throws Exception {
+        // arrange
+        Order order = orders.get(0);
+        OrderRequestDto request = new OrderRequestDto(null, order.getOrderItems().stream().map(oi -> new OrderItemRequestDto(oi.getBook().getId(), -1)).toList());
+
+
+        // act and assert
+        mockMvc.perform(post(baseUrl)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnprocessableContent());
+
+        verify(orderService, never()).createOrder(any(OrderRequestDto.class));
+    }
+}
