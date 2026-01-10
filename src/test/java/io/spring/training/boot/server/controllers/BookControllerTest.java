512bec4eb70d57c91dd6ae7bb9016a5de32f099b
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-10 14:01:00 +0200

    Test(BookControllerTest): test createBook method

--- src/test/java/io/spring/training/boot/server/controllers/BookControllerTest.java
+++ src/test/java/io/spring/training/boot/server/controllers/BookControllerTest.java
@@ -1,5 +1,7 @@
 package io.spring.training.boot.server.controllers;
 
+import io.spring.training.boot.server.DTOs.book.BookCreationResponseDto;
+import io.spring.training.boot.server.DTOs.book.BookRequestDto;
 import io.spring.training.boot.server.DTOs.book.BookSummaryDto;
 import io.spring.training.boot.server.config.StorageProperties;
 import io.spring.training.boot.server.exceptions.BookNotFoundException;
@@ -8,7 +10,9 @@ import io.spring.training.boot.server.models.Book;
 import io.spring.training.boot.server.models.Genre;
 import io.spring.training.boot.server.models.projections.BookWithStats;
 import io.spring.training.boot.server.services.BookService;
+import io.spring.training.boot.server.utils.mappers.AuthorMapper;
 import io.spring.training.boot.server.utils.mappers.BookMapper;
+import io.spring.training.boot.server.utils.mappers.GenreMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +21,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
 import tools.jackson.databind.ObjectMapper;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Set;
 
@@ -69,6 +76,11 @@ public class BookControllerTest {
         Book b1 = Book.builder()
                 .id(1L)
                 .title("Book One")
+                .description("Description One")
+                .price(new BigDecimal("29.99"))
+                .numberOfPages(300)
+                .stock(10)
+                .image("book-one.png")
                 .authors(Set.of(a1))
                 .genres(Set.of(new Genre("Horror")))
                 .build();
@@ -76,6 +88,11 @@ public class BookControllerTest {
         Book b2 = Book.builder()
                 .id(2L)
                 .title("Book Two")
+                .description("Description Two")
+                .price(new BigDecimal("39.99"))
+                .numberOfPages(400)
+                .stock(5)
+                .image("book-two.png")
                 .authors(Set.of(a2))
                 .genres(Set.of(new Genre("Drama")))
                 .build();
@@ -168,6 +185,93 @@ public class BookControllerTest {
                 .andExpect(status().isNotFound());
 
         verify(bookService).findBookById(eq(bookId));
+    }
+
+    @Test
+    public void givenValidBookRequestDtoAndImage_whenCreateBookIsCalled_thenReturnsCorrectBookCreationResponseDto() throws Exception {
+        // arrange
+        Book b = books.getFirst();
+        BookRequestDto request = new BookRequestDto(
+                b.getTitle(),
+                b.getDescription(),
+                b.getPrice(),
+                b.getNumberOfPages(),
+                b.getStock(),
+                Set.of(1L),
+                Set.of(1L));
+
+        BookCreationResponseDto response = new BookCreationResponseDto(
+                1L,
+                b.getTitle(),
+                b.getDescription(),
+                b.getPrice(),
+                b.getNumberOfPages(),
+                b.getStock(),
+                "book-image.png",
+                b.getAuthors().stream().map(AuthorMapper::toAuthorResponseDto).toList(),
+                b.getGenres().stream().map(GenreMapper::toGenreResponseDto).toList(),
+                List.of()
+        );
+
+        MockMultipartFile bookData = new MockMultipartFile(
+                "bookData",
+                "",
+                "application/json",
+                objectMapper.writeValueAsBytes(request)
+        );
+
+        MockMultipartFile bookImage = new MockMultipartFile(
+                "bookImage",
+                "book-image.png",
+                "image/png",
+                "fake-image-content".getBytes()
+        );
+
+        when(bookService.createBook(any(BookRequestDto.class), any(MultipartFile.class))).thenReturn(response);
+
+        // act and assert
+        mockMvc.perform(multipart(baseUrl)
+                        .file(bookData)
+                        .file(bookImage))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value(response.title()));
+
+        verify(bookService).createBook(any(BookRequestDto.class), any(MultipartFile.class));
+    }
+
+    @Test
+    public void givenInvalidBookRequestDto_whenCreateBookIsCalled_thenReturnsUnProcessableContentResponse() throws Exception {
+        // arrange
+        Book b = books.getFirst();
+        BookRequestDto request = new BookRequestDto(
+            "",
+                b.getDescription(),
+                b.getPrice(),
+                b.getNumberOfPages(),
+                b.getStock(),
+                Set.of(1L),
+                Set.of(1L));
+
+        MockMultipartFile bookData = new MockMultipartFile(
+                "bookData",
+                "",
+                "application/json",
+                objectMapper.writeValueAsString(request).getBytes());
+
+        MockMultipartFile bookImage = new MockMultipartFile(
+                "bookImage",
+                "book-image.png",
+                "image/png",
+                "fake-image-content".getBytes()
+        );
+
+        // act and assert
+        mockMvc.perform(multipart(baseUrl)
+                        .file(bookData)
+                        .file(bookImage))
+                .andExpect(status().isUnprocessableContent());
+
+        verify(bookService, never()).createBook(any(BookRequestDto.class), any(MultipartFile.class));
     }
 
 }
