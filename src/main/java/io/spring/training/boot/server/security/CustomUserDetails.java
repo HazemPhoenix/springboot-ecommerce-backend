2d99c5fb455abfad381e0a0df7b81c1914f3bb60
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-12 16:17:56 +0200

    Feat(CustomUserDetails): create the UserDetails implementation to apply the adapter pattern to the User model

---
+++ src/main/java/io/spring/training/boot/server/security/CustomUserDetails.java
@@ -0,0 +1,43 @@
+package io.spring.training.boot.server.security;
+
+import io.spring.training.boot.server.models.User;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails, CredentialsContainer {
+    private final User user;
+
+    @Override
+    public void eraseCredentials() {
+        user.setPassword("");
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName().name()))
+                .toList();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+}
