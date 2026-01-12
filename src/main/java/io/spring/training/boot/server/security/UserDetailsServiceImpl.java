3b40ebf63387250717f0178bc6b283744e89b7d6
HazemPhoenix <hazem.mando408@gmail.com>
2026-01-12 16:23:46 +0200

    Feat(UserDetailsServiceImpl): create the custom UserDetailsService

---
+++ src/main/java/io/spring/training/boot/server/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package io.spring.training.boot.server.security;
+
+import io.spring.training.boot.server.models.User;
+import io.spring.training.boot.server.repositories.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid Credentials."));
+
+        return new CustomUserDetails(user);
+    }
+}
